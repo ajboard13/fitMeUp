b80cc55872b77ef90a6e0b52ee2529f5808c50b2
ajboard13 <aaronjboard@live.com>
2017-08-08 19:29:49 -0400

    fixed the Github method to return the correct oath token

--- src/main/java/com/test/Oauth/Github.java
+++ src/main/java/com/test/Oauth/Github.java
@@ -3,14 +3,19 @@ package com.test.Oauth;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
-/**
- * Created by user on 8/8/2017.
+/*
+Aaron Board
  */
+
+
 public class Github {
     public static JSONObject getUserInfo(String token) {
         try {
@@ -34,28 +39,37 @@ public class Github {
         return null;
     }
 
-    public static String getOAuthToken(String code){
+    public static String getOAuthToken(String code) {
 
         //TODO always delete before a push
         String clientId = "562161c07d5f44826394";
         String clientSecret = "e64da38348ed61c3d58a4d157175db5c140a6d22";
         String accessToken = "";
+        StringBuilder sb = new StringBuilder();
+        URLConnection urlConn = null;
+        InputStreamReader in = null;
         try {
-            URL url = new URL("https://github.com/login/oauth/access_token?client_id="+clientId+"&client_secret="+clientSecret+"&code="+code);
-            HttpURLConnection con = (HttpURLConnection)(url.openConnection());
-            con.setInstanceFollowRedirects( false );
-            con.connect();
-            int responseCode = con.getResponseCode();
-            System.out.println( responseCode );
-            accessToken = con.getHeaderField("access_token");
-            System.out.println( accessToken );
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            URL url = new URL("https://github.com/login/oauth/access_token?client_id=" + clientId + "&client_secret=" + clientSecret + "&code=" + code);
+            urlConn = url.openConnection();
+            if (urlConn != null)
+                urlConn.setReadTimeout(60 * 1000);
+            if (urlConn != null && urlConn.getInputStream() != null) {
+                in = new InputStreamReader(urlConn.getInputStream(),
+                        Charset.defaultCharset());
+                BufferedReader reader = new BufferedReader(in);
+                for (String line; (line = reader.readLine()) != null; ) {
+                    sb.append(line);
+                }
+            }
+            in.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception while calling URL:", e);
         }
+        System.out.println(sb);
+
+        accessToken = sb.substring(sb.indexOf("=")
+                + 1, sb.indexOf("&"));
+
         return accessToken;
     }
 }
